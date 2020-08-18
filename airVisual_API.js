0517cef30ee72885c61e45367d4133c97a4b4fd6
Nick Birch <nick@birchstartups.com>
2020-08-18 10:35:00 -0400

    update

--- airVisual_API.js
+++ airVisual_API.js
@@ -5,26 +5,23 @@ let apiKey = "47a1b836-c4a4-4adf-8372-47be553d71b8";
 // Selectors
 const stateList = document.getElementById("state");
 
-
+// Do an initial fetch to grab the states to create a dropdown list
 fetch("http://api.airvisual.com/v2/states?country=USA&key=" + apiKey)
-  .then(response => response.json())
-  .then(result => {
+  .then((response) => response.json())
+  .then((result) => {
     console.log(result);
     populateDropdown(result);
-    })
-  .catch(error => console.log('error', error));
+  })
+  .catch((error) => console.log("error", error));
 
-function populateDropdown (result) {
-    let states = result.data.map(list => `${list.state}`);
-    console.log(states);
-states.forEach(element => {
-    let listItem = document.createElement('option');
+// Use DOM manipulation to inject states returned in inital fetch into the dropdown
+function populateDropdown(result) {
+  let states = result.data.map((list) => `${list.state}`);
+  console.log(states);
+  states.forEach((element) => {
+    let listItem = document.createElement("option");
     listItem.innerText = element;
     listItem.setAttribute("value", element);
     stateList.appendChild(listItem);
-});
+  });
 }
-
-
-
-
